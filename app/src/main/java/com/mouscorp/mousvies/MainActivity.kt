f9943a2e83369d7fe10d8b3391ef478d72b95e4e
trist <tristanmatteoli@gmail.com>
2020-11-16 00:08:04 +0100

    Premiers test d'implémentations du recyclerview dans les fragments

--- app/src/main/java/com/mouscorp/mousvies/MainActivity.kt
+++ app/src/main/java/com/mouscorp/mousvies/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.mouscorp.mousvies
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.mouscorp.mousvies.api.adapter.MovieRecyclerViewAdapter
+import com.mouscorp.mousvies.api.models.Movie
+
 
 class MainActivity : AppCompatActivity() {
+
+
+
+    companion object {
+        const val SECRET_API_KEY = "c4e28b5de3a34ac158059130ce07705d"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/mouscorp/mousvies/api/models/SpokenLanguage.kt
@@ -0,0 +1,5 @@
+package com.mouscorp.mousvies.api.models
+
+class SpokenLanguage (){
+
+}
