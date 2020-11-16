ade505ed768ad8bc0abaa2c7ddfe6d5e609e2195
trist <tristanmatteoli@gmail.com>
2020-11-16 17:33:56 +0100

    Ajou appli finale

---
+++ app/src/main/java/com/mouscorp/mouvies/api/models/MovieDetails.kt
@@ -0,0 +1,25 @@
+package com.mouscorp.mouvies.api.models
+
+import com.google.gson.annotations.SerializedName
+
+// On peut utiliser les annotations comme ci-dessous ou alors les data class
+class MovieDetails (
+    @SerializedName("title") var title : String,
+    @SerializedName("original_title") var originalTitle : String,
+    @SerializedName("budget") var budget : Int,
+    @SerializedName("original_language") var original_language : String,
+    @SerializedName("poster_path") var poster_path : String,
+    @SerializedName("popularity") var popularity : Number,
+    @SerializedName("overview") var overview : String?,
+    @SerializedName("vote_average") var vote_average : Number,
+    @SerializedName("vote_count") var vote_count : Int,
+    @SerializedName("status") var status : String,
+    @SerializedName("spoken_languages") var spoken_languages : List<SpokenLanguage>,
+    @SerializedName("revenue") var revenue : Int,
+    @SerializedName("genres") var genres : List<Genre>,
+    @SerializedName("release_date") var release_date : String,
+    @SerializedName("production_countries") var production_countries : Int,
+    @SerializedName("production_companies") var production_companies : Int
+){
+
+}
---
+++ app/src/main/java/com/mouscorp/mouvies/api/models/SpokenLanguage.kt
@@ -0,0 +1,5 @@
+package com.mouscorp.mouvies.api.models
+
+class SpokenLanguage (){
+
+}
